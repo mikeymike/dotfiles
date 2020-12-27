a098518b626fa4682847953a706a46cc6118c7ac
mikeymike <mikeymike.mw@gmail.com>
2020-12-27 07:31:59 +0000

    Put alacritty config in the right place

--- terminal/install.sh
+++ terminal/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Running terminal install"
-echo "cp $DOTFILES_ROOT/terminal/alacritty.yml $HOME/alacritty.yml"
+echo "cp $DOTFILES_ROOT/terminal/alacritty.yml $HOME/.alacritty.yml"
 cp $DOTFILES_ROOT/terminal/alacritty.yml $HOME/alacritty.yml
 
 curl https://raw.githubusercontent.com/alebelcor/alacritty-snazzy/master/snazzy.yml >> $HOME/alacritty.yml
