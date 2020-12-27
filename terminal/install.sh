fb6fc58d554bb6957d69b449be5268a7893fad7b
mikeymike <mikeymike.mw@gmail.com>
2020-12-27 09:29:56 +0000

    Take 2 on alacritty move

--- terminal/install.sh
+++ terminal/install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 echo "Running terminal install"
-echo "cp $DOTFILES_ROOT/terminal/alacritty.yml $HOME/.alacritty.yml"
-cp $DOTFILES_ROOT/terminal/alacritty.yml $HOME/alacritty.yml
+cp $DOTFILES_ROOT/terminal/alacritty.yml $HOME/.alacritty.yml
 
 curl https://raw.githubusercontent.com/alebelcor/alacritty-snazzy/master/snazzy.yml >> $HOME/alacritty.yml
