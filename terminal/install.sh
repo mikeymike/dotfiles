fb980dc8a43ff953ad1da21a477b7c79e33d6dbd
mikeymike <mikeymike.mw@gmail.com>
2020-12-28 00:15:30 +0000

    Use cwd as base for alacritty cp

--- terminal/install.sh
+++ terminal/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Running terminal install"
-cp $DOTFILES_ROOT/terminal/alacritty.yml $HOME/.alacritty.yml
+cp terminal/alacritty.yml $HOME/.alacritty.yml
 
 curl https://raw.githubusercontent.com/alebelcor/alacritty-snazzy/master/snazzy.yml >> $HOME/alacritty.yml
