51195470e8f3eac591900ff5d3fb86bf7cd1ba9a
Michael Woodward <mikeymike.mw@gmail.com>
2021-04-16 07:56:14 +0100

    More robust PHP aliases

--- php/aliases.zsh
+++ php/aliases.zsh
@@ -1,4 +1,4 @@
 #!/bin/sh
-alias php72="/usr/local/Cellar/php@7.2/$(brew ls --versions php@7.2 | awk '{ print $2 }')/bin/php"
-alias php73="/usr/local/Cellar/php@7.3/$(brew ls --versions php@7.3 | awk '{ print $2 }')/bin/php"
-alias php74="/usr/local/Cellar/php@7.4/$(brew ls --versions php@7.4 | awk '{ print $2 }')/bin/php"
+alias php72="/usr/local/opt/php@7.2/bin/php"
+alias php73="/usr/local/opt/php@7.3/bin/php"
+alias php74="/usr/local/opt/php@7.4/bin/php"
