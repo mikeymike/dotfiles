84f710dc2defa7688eb20558b7effbab7e7654b9
Michael Woodward <mikeymike.mw@gmail.com>
2021-05-26 07:51:59 +0100

    Fixup PHP aliases

--- php/aliases.zsh
+++ php/aliases.zsh
@@ -1,4 +1,2 @@
 #!/bin/sh
-alias php72="/usr/local/opt/php@7.2/bin/php"
-alias php73="/usr/local/opt/php@7.3/bin/php"
-alias php74="/usr/local/opt/php@7.4/bin/php"
+alias php74="/opt/homebrew/opt/php@7.4/bin/php"
